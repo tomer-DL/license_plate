6abbf855ab56701831bac44947c96cdacd5752aa
Tomer Cnaan <cnaant73@gmail.com>
2018-06-20 23:35:44 +0300

    Update train_lp.py

--- train_lp.py
+++ train_lp.py
@@ -23,8 +23,13 @@ model.add(Conv2D(64, (3, 3), padding="same", activation='relu'))
 model.add(MaxPool2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
+model.add(Conv2D(128, (3, 3), padding="same", activation='relu'))
+model.add(Conv2D(128, (3, 3), padding="same", activation='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(4, activation='relu'))
 
